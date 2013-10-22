493e1798a4d2605fa2a1a21f5aa5cf885d8ee25e
sauloaalmeida <sauloandrade@yahoo.com>
2013-10-22 19:11:40 -0200

    adicionando mais debugs nos metodos

--- fft/fft.c
+++ fft/fft.c
@@ -282,34 +282,46 @@ void fftLegivel(float data[], unsigned long qtdElementos, int isign){
         printf("Array de entrada: ");
         imprimeVetor(data,tamArray);
 	j=1;
+        printf("j=1\n");
 
-	printf("inicio rotina de bit reverso\n");
-        printf("--------------------------\n");
+	printf(">> inicio rotina de bit reverso\n");
 	for (i=1;i<tamArray;i+=2) {
-                printf("loop bit reverso: j=%lu, i=%lu\n",j,i);
+                printf("   >> loop bit reverso: j=%lu, i=%lu\n",j,i);
 		if (j > i) {
-                        printf("loop bitreverso: entrou no IF: troca i=%lu(real:%f;imag:%f), com j=%lu(real:%f;imag:%f)\n",i,data[i],data[i+1],j,data[j],data[j+1]);
+                        printf("   loop bitreverso: entrou no IF: troca i=%lu(real:%f;imag:%f), com j=%lu(real:%f;imag:%f)\n",i,data[i],data[i+1],j,data[j],data[j+1]);
 			SWAP(data[j],data[i]);
 			SWAP(data[j+1],data[i+1]);
 		}
 		m=qtdElementos;
-                printf("\nm=%lu\n",m);
+                printf("\n   m=%lu\n",m);
 		while (m >= 2 && j > m) {
-			printf("bitreverso loop while: m\n ");
-			
+			printf("      bitreverso loop while: m=%lu j=%lu \n",m,j);			
 			j = j-m;
 			m = m/2;
+			printf("         bitreverso loop while apos atualizacao: m=%lu j=%lu \n",m,j);
 		}
+                printf("   bitreverso depois do  while: m=%lu j=%lu \n",m,j);
 		j = j+m;
+		printf("   bitreverso depois do while, com j atualizado: j = j+m; j=%lu \n",j);
+                
 	}
-        printf("--------------------------\n");
-        printf("fim rotina de bit reverso\n\n");
+        printf("<< fim rotina de bit reverso\n\n");
+
+
+        printf(">> inicio rotina de calculo do fourier\n");
 
 	mmax=2; //divide em 2 blocos (pares e impares)
-	while (tamArray > mmax) {
 
+	while (tamArray > mmax) {
+        printf("   >> inicio do loop externo do calculo do fourier\n");
+                printf("   mmax=2\n");
+                printf("   istep=mmax * 2;");
 		istep=mmax * 2; //tamanho do bloco
+                printf("   istep=%lu\n",istep);
+
+		printf("   theta=isign * (6.28318530717959/mmax);");
 		theta=isign * (6.28318530717959/mmax); // theta = signal * (pi/2) 1 iteracao = pi 3,141543
+                printf("   theta=%f\n",theta);
 
 		wtemp=sin(0.5*theta); //wtemp = seno(3,141543/2)
 		wpr = -2.0*wtemp*wtemp; //wpr = -2.wtemp^2
@@ -317,7 +329,9 @@ void fftLegivel(float data[], unsigned long qtdElementos, int isign){
 		wr=1.0; //wr = 1
 		wi=0.0; //wi = 0
 		for (m=1;m<mmax;m+=2) {
+		printf("      >> inicio do for interno 1 - for (m=1;m<mmax;m+=2) m=%lu, mmax=%lu\n",m,mmax);
 			for (i=m;i<=tamArray;i+=istep) {
+                        printf("         >> inicio do for interno 2 - for (i=m;i<=tamArray;i+=istep) i=%lu, m=%lu, istep=%lu\n",i,m,istep);
 				j=i+mmax;
 				tempr = wr*data[j]-wi*data[j+1];
 				tempi = wr*data[j+1]+wi*data[j];
@@ -325,24 +339,40 @@ void fftLegivel(float data[], unsigned long qtdElementos, int isign){
 				data[j+1] = data[i+1]-tempi;
 				data[i] = data[i] +tempr;
 				data[i+1] = data[i+1] + tempi;
+                        printf("         >> final do for interno 2\n");
 			}
+			printf("      wr = (wtemp=wr)*wpr-(wi*wpi)+wr; - wr=%f,wpi=%f,wi=%f,wpr=%f,wtemp=%f => ",wr,wpi,wi,wpr,wtemp);
 			wr = (wtemp=wr)*wpr-(wi*wpi)+wr;
+			printf("      wr=%f\n",wr);
+ 
+                        printf("      wi = (wi*wpr)+(wtemp*wpi)+wi; - wi=%f,wpi=%f,wtemp=%f,wpr=%f => ",wi,wpi,wtemp,wpr);
 			wi = (wi*wpr)+(wtemp*wpi)+wi;
+                        printf("      wi=%f\n",wi);
+                        
 		}
+                printf("      << final do for interno 1\n");
+                printf("   mmax=istep;");
 		mmax=istep;//caminha o proximo passo no laco mais externo
+                printf("  mmax=%lu\n",mmax);
+        printf("   << final do loop externo do calculo do fourier\n");
+
+
 	}
+
+        printf("<< final da rotina de calculo do fourier\n");
+
 }
 
 
 int main(void) {
 
-		float data[16] = {0.,0.,1.,0.,2.,0.,3.,0.,0.,0.,1.,0.,2.,0.,3.,0.} ;
-		fftLegivel(data-1,8,1);
-		imprimeVetor(data,16);
+		//float data[16] = {0.,0.,1.,0.,2.,0.,3.,0.,0.,0.,1.,0.,2.,0.,3.,0.} ;
+		//fftLegivel(data-1,8,1);
+		//imprimeVetor(data,16);
 
-		//float data[8] = {0.,0.,1.,0.,2.,0.,3.,0.};
-		//fftLegivel(data-1,4,1); 
-		//imprimeVetor(data,8);
+		float data[8] = {0.,0.,1.,0.,2.,0.,3.,0.};
+		fftLegivel(data-1,4,1); 
+		imprimeVetor(data,8);
 
 		return 0;
 
