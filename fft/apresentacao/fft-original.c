ddce17ddf99878342e3ede084f56cbd10cacf3ae
Saulo Andrade <sauloandrade@gmail.com>
2013-10-31 00:26:41 -0200

    melhorando codigo da apresentacao

--- fft/apresentacao/fft-original.c
+++ fft/apresentacao/fft-original.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#define SWAP(a,b) tempr=a;a=b;b=tempr
+#define SWAP(a,b) tempr=(a);(a)=(b);(b)=tempr
 
-void four1(float data[], unsigned long nn, int isign){
+/******************************************************************************/
+void four1(double data[], unsigned long nn, int isign)
+/*******************************************************************************
+Replaces data[1..2*nn] by its discrete Fourier transform, if isign is input as
+1; or replaces data[1..2*nn] by nn times its inverse discrete Fourier transform,
+if isign is input as -1.  data is a complex array of length nn or, equivalently,
+a real array of length 2*nn.  nn MUST be an integer power of 2 (this is not
+checked for!).
+*******************************************************************************/
+{
 	unsigned long n,mmax,m,j,istep,i;
 	double wtemp,wr,wpr,wpi,wi,theta;
-	float tempr,tempi;
+	double tempr,tempi;
+
 	n=nn << 1;
 	j=1;
-	for (i=1;i<n;i+=2) {
+	for (i=1;i<n;i+=2) { /* This is the bit-reversal section of the routine. */
 		if (j > i) {
-			SWAP(data[j],data[i]);
+			SWAP(data[j],data[i]); /* Exchange the two complex numbers. */
 			SWAP(data[j+1],data[i+1]);
 		}
 		m=nn;
@@ -21,18 +29,19 @@ void four1(float data[], unsigned long nn, int isign){
 		}
 		j += m;
 	}
+
 	mmax=2;
-	while (n > mmax) {
+	while (n > mmax) { /* Outer loop executed log2 nn times. */
 		istep=mmax << 1;
-		theta=isign*(6.28318530717959/mmax);
+		theta=isign*(6.28318530717959/mmax); /* Initialize the trigonometric recurrence. */
 		wtemp=sin(0.5*theta);
 		wpr = -2.0*wtemp*wtemp;
 		wpi=sin(theta);
 		wr=1.0;
 		wi=0.0;
-		for (m=1;m<mmax;m+=2) {
+		for (m=1;m<mmax;m+=2) { /* Here are the two nested inner loops. */
 			for (i=m;i<=n;i+=istep) {
-				j=i+mmax;
+				j=i+mmax; /* This is the Danielson-Lanczos formula. */
 				tempr=wr*data[j]-wi*data[j+1];
 				tempi=wr*data[j+1]+wi*data[j];
 				data[j]=data[i]-tempr;
@@ -40,7 +49,7 @@ void four1(float data[], unsigned long nn, int isign){
 				data[i] += tempr;
 				data[i+1] += tempi;
 			}
-			wr=(wtemp=wr)*wpr-wi*wpi+wr;
+			wr=(wtemp=wr)*wpr-wi*wpi+wr; /* Trigonometric recurrence. */
 			wi=wi*wpr+wtemp*wpi+wi;
 		}
 		mmax=istep;
@@ -49,8 +58,6 @@ void four1(float data[], unsigned long nn, int isign){
 
 int main(void) {
 		float data[8] = {0.,0.,1.,0.,2.,0.,3.,0.};
-		fftLegivel(data-1,4,1); 
+		four1(data-1,4,1); 
 		return 0;
 }
-
-
--- fft/apresentacao/fft-otimizada.c
+++ fft/apresentacao/fft-otimizada.c
@@ -1,63 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
 #define SWAP(a,b) tmp=a;a=b;b=tmp
 
-void fft(float data[], unsigned long qtdElementos, int isign){
+/******************************************************************************/
+void four1(double data[], unsigned long nn, int isign)
+/*******************************************************************************
+Replaces data[1..2*nn] by its discrete Fourier transform, if isign is input as
+1; or replaces data[1..2*nn] by nn times its inverse discrete Fourier transform,
+if isign is input as -1.  data is a complex array of length nn or, equivalently,
+a real array of length 2*nn.  nn MUST be an integer power of 2 (this is not
+checked for!).
+*******************************************************************************/
+{
+	unsigned long n,mmax,j,i;
+	double wtemp,wr,wpr,wpi,wi,theta;
+	double tmp;
 
-	unsigned long tamArray,mmax,j,i;
-	double wtemp,wr,wpr,wpi,wi;
-    float tmp;
-
-	tamArray=qtdElementos<<1;
-		
+	n=nn << 1;
 	j=1;
-
-	for (i=1;i<tamArray;i+=2) {
-                unsigned long m;
+	for (i=1;i<n;i+=2) { /* This is the bit-reversal section of the routine. */
 		if (j > i) {
-			SWAP(data[j],data[i]);
+			SWAP(data[j],data[i]); /* Exchange the two complex numbers. */
 			SWAP(data[j+1],data[i+1]);
 		}
-		m=qtdElementos;
+		m=nn;
 		while (m >= 2 && j > m) {
-			j = j-m;
-			m = m/2;
+			j -= m;
+			m >>= 1;
 		}
-		j = j+m;               
+		j += m;
 	}
 
-
 	mmax=2;
-	while (tamArray > mmax) {
-        unsigned long m,i;
+	while (n > mmax) { /* Outer loop executed log2 nn times. */
+	    unsigned long m,i;
 		unsigned long istep=mmax << 1;
-		double theta=isign * (6.28318530717959/mmax);
+		double theta=isign*(6.28318530717959/mmax); /* Initialize the trigonometric recurrence. */
 		wtemp=sin(0.5*theta);
 		wpr = -2.0*wtemp*wtemp;
 		wpi=sin(theta);
 		wr=1.0;
 		wi=0.0;
-		for (m=1;m<mmax;m+=2) {
-			for (i=m;i<=tamArray;i+=istep) {				
-				j=i+mmax;
-				float tempr = wr*data[j]-wi*data[j+1];
-				float tempi = wr*data[j+1]+wi*data[j];
-				data[j] = data[i]-tempr;
-				data[j+1] = data[i+1]-tempi;
-				data[i] = data[i]+tempr;
-				data[i+1] = data[i+1] + tempi;
+		for (m=1;m<mmax;m+=2) { /* Here are the two nested inner loops. */
+			for (i=m;i<=n;i+=istep) {
+				j=i+mmax; /* This is the Danielson-Lanczos formula. */
+				float tempr=wr*data[j]-wi*data[j+1];
+				float tempi=wr*data[j+1]+wi*data[j];
+				data[j]=data[i]-tempr;
+				data[j+1]=data[i+1]-tempi;
+				data[i] += tempr;
+				data[i+1] += tempi;
 			}
-			wr = (wtemp=wr)*wpr-(wi*wpi)+wr;
-			wi = (wi*wpr)+(wtemp*wpi)+wi;
-		}                
+			wr=(wtemp=wr)*wpr-wi*wpi+wr; /* Trigonometric recurrence. */
+			wi=wi*wpr+wtemp*wpi+wi;
+		}
 		mmax=istep;
 	}
 }
 
 int main(void) {
 		float data[8] = {0.,0.,1.,0.,2.,0.,3.,0.};
-		fftLegivel(data-1,4,1); 
+		four1(data-1,4,1); 
 		return 0;
 }
 
