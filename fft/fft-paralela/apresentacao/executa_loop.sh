fc1383826c151c21ae612d106a7f49bf5218beef
Saulo Andrade <sauloandrade@gmail.com>
2013-12-05 17:32:49 -0200

    corrigindo shellscripts para executar codigos

--- fft/fft-paralela/apresentacao/executa_loop.sh
+++ fft/fft-paralela/apresentacao/executa_loop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm fft-versao8
+gcc -O1 -lm -lpthread -o fft-versao8 fft-versao8.c
+
 maxQtdThreads=4
 maxElementos=25
 for ((t=2; t<=$maxQtdThreads; t=t*2 ))
--- fft/fft-paralela/versao2/executa_loop.sh
+++ fft/fft-paralela/versao2/executa_loop.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-rm fft-thread
-gcc -O1 -lm -lpthread -o fft-thread fft-thread.c
-./fft-thread
+rm fft2
+gcc -O1 -lm -lpthread -o fft2 fft2.c
+
+maxQtdThreads=4
+maxElementos=25
+for ((t=2; t<=$maxQtdThreads; t=t*2 ))
+do
+     for ((i=2; i<=$maxElementos; ++i ))
+     do
+	 for ((p=1; p<=$i-1; ++p ))
+         do
+              ./fft-versao8 $(( 2 ** i )) $t $p
+         done
+     done
+done
